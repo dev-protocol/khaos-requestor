dcca65d530d3b5ab2134aff6ff62edda40a1c586
Stuart Kuentzel <s.w.kuentzel@gmail.com>
2021-11-30 16:25:35 -0500

    init

---
+++ src/main.rs
@@ -0,0 +1,135 @@
+use reqwest::{
+    header::{HeaderMap, HeaderName, HeaderValue},
+    Url,
+};
+use rocket::{
+    response::status::BadRequest,
+    routes,
+    serde::{json::Json, Deserialize, Serialize},
+};
+use std::{collections::HashMap, str::FromStr};
+
+#[cfg(test)]
+mod tests;
+
+#[macro_use]
+extern crate rocket;
+
+#[derive(Deserialize, Serialize)]
+enum SecretLocation {
+    QueryParam,
+    Header,
+    None,
+}
+
+#[derive(Deserialize, Serialize)]
+struct KhaosRequest<'r> {
+    // The associated ethereum address
+    contract: &'r str,
+    // Associated solidity callback function
+    callback: &'r str,
+    // Url endpoint to query
+    destination: &'r str,
+    // destination_query: Vec<&'r str>,
+    destination_query: HashMap<String, String>,
+    destination_parse_response: Vec<&'r str>,
+    // Should we set the secret in the headers or the URL?
+    secret_location: SecretLocation,
+    secret_key: &'r str,
+    // Developer Server Url
+    require: &'r str,
+    require_query: Vec<&'r str>,
+    require_parse_response: &'r str,
+}
+
+#[derive(Deserialize, Serialize)]
+struct TestResp {
+    val: String,
+}
+
+#[post("/", format = "application/json", data = "<req>")]
+async fn handler(
+    req: Json<KhaosRequest<'_>>,
+) -> Result<Json<serde_json::Value>, BadRequest<String>> {
+    // Fetch key from developer server
+    let key = match retreive_key(req.require, req.require_parse_response).await {
+        Ok(key) => key,
+        Err(error) => return Err(BadRequest(Some(error))),
+    };
+
+    println!("key is {}", key.to_string());
+
+    let mut query_params = req.destination_query.clone();
+    let client = reqwest::Client::new();
+    let mut headers = HeaderMap::new();
+
+    match req.secret_location {
+        SecretLocation::QueryParam => {
+            query_params.insert(req.secret_key.to_string(), key);
+        }
+        SecretLocation::Header => {
+            let header_val = match HeaderValue::from_str(&key) {
+                Ok(val) => val,
+                Err(error) => return Err(BadRequest(Some(error.to_string()))),
+            };
+
+            let header_name = match HeaderName::from_str(req.secret_key) {
+                Ok(val) => val,
+                Err(error) => return Err(BadRequest(Some(error.to_string()))),
+            };
+
+            headers.insert(header_name, header_val);
+            headers = HeaderMap::new()
+        }
+        SecretLocation::None => {
+            println!("No secret needed for request");
+        }
+    };
+
+    // Format URL with params
+    let url = match Url::parse_with_params(req.destination, query_params) {
+        Ok(url) => url,
+        Err(error) => return Err(BadRequest(Some(error.to_string()))),
+    };
+
+    // Make external Query
+    let query = match client.get(url).headers(headers).send().await {
+        Ok(val) => val,
+        Err(error) => return Err(BadRequest(Some(error.to_string()))),
+    };
+
+    // Parse returned JSON
+    let json: serde_json::Value = match query.json().await {
+        Ok(json) => json,
+        Err(error) => return Err(BadRequest(Some(error.to_string()))),
+    };
+
+    Ok(Json(json))
+}
+
+async fn retreive_key(
+    dev_server_url: &str,
+    require_parse_response: &str,
+) -> Result<String, String> {
+    let dev_res = match reqwest::get(dev_server_url).await {
+        Ok(val) => val,
+        Err(error) => return Err(error.to_string()),
+    };
+
+    let json: serde_json::Value = match dev_res.json().await {
+        Ok(json) => json,
+        Err(error) => return Err(error.to_string()),
+    };
+
+    let key: String = match json.get(&require_parse_response) {
+        Some(val) => val.to_string(),
+        None => return Err("Error fetching require_parse_response from json".to_string()),
+    };
+
+    Ok(key)
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build().mount("/", routes![handler])
+}
---
+++ src/tests.rs
@@ -0,0 +1,65 @@
+use super::rocket;
+use rocket::http::{ContentType, Status};
+use rocket::local::blocking::Client;
+
+use crate::{KhaosRequest, TestResp};
+
+#[test]
+fn handler() {
+    let client = Client::tracked(rocket()).expect("valid rocket instance");
+
+    let req = KhaosRequest {
+        contract: "",
+        callback: "",
+        destination: "",
+        destination_query: vec![""],
+        destination_parse_response: vec![""],
+        require: "https://chain.so/api/v2/get_info/BTC",
+        require_query: vec![""],
+        require_parse_response: "status",
+    };
+
+    let serialized = serde_json::to_string(&req).unwrap();
+
+    let response: TestResp = match client
+        .post("/")
+        .header(ContentType::JSON)
+        .body(serialized)
+        .dispatch()
+        .into_json()
+    {
+        Some(thing) => thing,
+        None => TestResp {
+            val: "".to_string(),
+        },
+    };
+
+    let formatted_val = format!("{}", response.val).replace("\"", "");
+
+    assert_eq!(formatted_val, "success");
+}
+
+// #[test]
+// fn hello_mir() {
+//     let client = Client::tracked(super::rocket()).unwrap();
+//     let response = client.get("/hello/%D0%BC%D0%B8%D1%80").dispatch();
+//     assert_eq!(response.into_string(), Some("Привет, мир!".into()));
+// }
+
+// #[test]
+// fn wave() {
+//     let client = Client::tracked(super::rocket()).unwrap();
+//     for &(name, age) in &[("Bob%20Smith", 22), ("Michael", 80), ("A", 0), ("a", 127)] {
+//         let uri = format!("/wave/{}/{}", name, age);
+//         let real_name = RawStr::new(name).percent_decode_lossy();
+//         let expected = format!("👋 Hello, {} year old named {}!", age, real_name);
+//         let response = client.get(uri).dispatch();
+//         assert_eq!(response.into_string().unwrap(), expected);
+
+//         for bad_age in &["1000", "-1", "bird", "?"] {
+//             let bad_uri = format!("/wave/{}/{}", name, bad_age);
+//             let response = client.get(bad_uri).dispatch();
+//             assert_eq!(response.status(), Status::NotFound);
+//         }
+//     }
+// }
